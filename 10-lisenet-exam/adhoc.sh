da38401d55aa6c86282181637896720a1ce7b4b2
michten <50970761+michten@users.noreply.github.com>
2021-06-30 20:01:07 +0000

    Lisenet: adhoc

--- 10-lisenet-exam/adhoc.sh
+++ 10-lisenet-exam/adhoc.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#
+# check:
+# ansible all -m ping -u root
 
-ansible ...
+ansible all -m user -a 'name=automation state=present shell=/bin/bash' -u root
+# quotes are important here. In -a outside have to be double quotes!!!
+ansible all -m authorized_key -a "user=automation key={{ lookup('file', '/home/automation/.ssh/automation2nodes.pub') }} state=present comment=automation2nodes" -u root
+ansible all -m copy -a "content='automation ALL=(ALL:ALL) NOPASSWD: ALL' dest=/etc/sudoers.d/automation" -u root
